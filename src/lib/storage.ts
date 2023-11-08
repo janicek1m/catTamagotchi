a2b67466a127a3e01692c2c03254e3cd61ce3cf7
Janice Kim <jankim@seas.upenn.edu>
2023-11-08 04:57:06 -0500

    updated names

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,18 +1,18 @@
-let allItems = new Set<string>(); // for resetting only asciigotchi local storage data
+let allItems = new Set<string>(); // for resetting only catTamagotchi local storage data
 
 export const save = (name: string, value: number) => {
-  localStorage.setItem(`asciigotchi-${name}`, String(value));
-  if (name === 'darkmode') return; // don't reset darkmode setting
-  allItems.add(`asciigotchi-${name}`);
+  localStorage.setItem(`catTamagotchi-${name}`, String(value));
+  if (name === "darkmode") return; // don't reset darkmode setting
+  allItems.add(`catTamagotchi-${name}`);
 };
 
 export const load = (name: string): number | undefined => {
-  let data = localStorage.getItem(`asciigotchi-${name}`);
+  let data = localStorage.getItem(`catTamagotchi-${name}`);
   if (data !== null || undefined) return Number(data);
 };
 
 export const reset = () => {
-  allItems.forEach(item => {
+  allItems.forEach((item) => {
     localStorage.removeItem(item);
   });
 };
